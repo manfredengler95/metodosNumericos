e7842147778a3c3f83fed22c129a698096095c67
ShadowNightmare <mabrizzio1@gmail.com>
2018-11-15 14:00:25 -0300

    Auto stash before merge of "develop" and "origin/develop"

--- calcDist.py
+++ calcDist.py
@@ -1,24 +1,36 @@
 def calcDist(tamObjSensor, distFocalLente, tamObjReal):
-
-    distNodalObj = (tamObjReal * distFocalLente)/ tamObjSensor
-    #Está de más decir que deben estar en las mismas unidades  (mm izq y m der  ref: link 8 en documentos_relacionados)
-    #Debe cumplir la relación x/f = X/d  (con x tamaño del objeto
-    #en el sensor, f distancia focal del lente, X tamaño real del
+    distNodalObj = (tamObjReal * distFocalLente) / tamObjSensor
+    # Está de más decir que deben estar en las mismas unidades  (mm izq y m der  ref: link 8 en documentos_relacionados)
+    # Debe cumplir la relación x/f = X/d  (con x tamaño del objeto
+    # en el sensor, f distancia focal del lente, X tamaño real del
     # objeto y d distancia desde el punto nodal al objeto  )
     return distNodalObj
+
 
 def calcTamañoReal(tamObjSensor, distFocalLente, distNodalObj):
     tamañoReal = (tamObjSensor * distNodalObj) / distFocalLente
     return tamañoReal
 
+
 def calcDistFocalLente(tamObjSensor, distNodalObj, tamObjReal):
     distFocalLente = (tamObjSensor * distNodalObj) / tamObjReal
     return distFocalLente
+
+
+def calcTamPix(tamReal, distancia, lenteFocal):
+    tamaPix = (tamReal * lenteFocal) / distancia
+    return tamaPix
+
+
 def verificarRelacion(tamObjSensor, distFocalLente, distNodalObj, tamObjReal):
-    if ((tamObjSensor / distFocalLente) == (tamObjReal / distNodalObj)):
-        print "Se cumple la relación"
-        print (tamObjSensor / distFocalLente) + " == " + (tamObjReal / distNodalObj)
+    if (tamObjSensor / distFocalLente) == (tamObjReal / distNodalObj):
+        print("Se cumple la relación")
+        print((tamObjSensor / distFocalLente) + " == " + (tamObjReal / distNodalObj))
     else:
-        print "No se cumple la relación"
-        print (tamObjSensor / distFocalLente) + " =/= " + (tamObjReal / distNodalObj)
+        print("No se cumple la relación")
+        print((tamObjSensor / distFocalLente) + " =/= " + (tamObjReal / distNodalObj))
     return
+
+# 7.7 cm tamReal
+# 50 cm Dist
+#
--- main.py
+++ main.py
@@ -6,3 +6,5 @@ import minrect
 #capturarvideo.mostrar()
 medirdistancia.distacia()
 #minrect._bounding_box_of(0.004)
+
+capturarvideo.mostrar()
