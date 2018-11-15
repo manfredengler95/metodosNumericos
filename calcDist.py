9fff428f087a15f15cc7087c54d86462ca4a4a00
Mauricio Abarca <mabrizzio1@gmail.com>
2018-11-15 13:13:48 -0300

    Añadido Modulo calcDist que contiene funciones para el calculo de distancia de un objeto, calculo de tamaño real, y verificacion de la relacion ocupada para dichos calculos

---
+++ calcDist.py
@@ -0,0 +1,21 @@
+def calcDist(tamObjSensor, distFocalLente, tamObjReal):
+
+    distNodalObj = (tamObjReal * distFocalLente)/ tamObjSensor
+    #Está de más decir que deben estar en las mismas unidades  (mm izq y m der  ref: link 8 en documentos_relacionados)
+    #Debe cumplir la relación x/f = X/d  (con x tamaño del objeto
+    #en el sensor, f distancia focal del lente, X tamaño real del
+    # objeto y d distancia desde el punto nodal al objeto  )
+    return distNodalObj
+
+def calcTamañoReal(tamObjSensor, distFocalLente, distNodalObj):
+    tamañoReal = (tamObjSensor * distNodalObj) / distFocalLente
+    return tamañoReal
+
+def verificarRelacion(tamObjSensor, distFocalLente, distNodalObj, tamObjReal):
+    if ((tamObjSensor / distFocalLente) == (tamObjReal / distNodalObj)):
+        print "Se cumple la relación"
+        print (tamObjSensor / distFocalLente) + " == " + (tamObjReal / distNodalObj)
+    else:
+        print "No se cumple la relación"
+        print (tamObjSensor / distFocalLente) + " =/= " + (tamObjReal / distNodalObj)
+    return
