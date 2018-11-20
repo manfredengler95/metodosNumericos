3458f7b645b0a6ab315e2dd0483aac6aebff5f12
manfred <manfred.engler.95@gmail.com>
2018-11-20 16:22:48 -0300

    Auto stash before rebase of "metodosNumericos/develop"

--- medirdistancia.py
+++ medirdistancia.py
@@ -30,11 +30,11 @@ def distacia():
 
         for pic, contour in enumerate(contours):
             area = cv2.contourArea(contour) #funcion de opencv que obtiene los contornos
-            if(area>300):
+            if(area>200):
                 x,y,w,h = cv2.boundingRect(contour) #Encuentra coordenadas de los contornos.
                 #print("Tam pix: " + str(x) + " " +  str(y))
 
-                a , b = str(round(calcDist.calcDist(x,1000,77)) ),str(round(calcDist.calcDist(y,20,77)))
+                a , b = str(round(calcDist.calcDist(x,1000,45)) ),str(round(calcDist.calcDist(y,20,45)))
                 img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
                 cv2.putText(img,"[" + str(x) + "," +  str(y) + "]px " + "[" + a + "," + b+ "]mm " ,(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
 
